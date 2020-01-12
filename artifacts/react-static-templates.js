44e3fc5dee9eca970e5b1a2e42eae09c31f7c27e
zhi zeng <cursorzz@163.com>
2020-01-12 20:45:13 +0800

    remove useless files that generated by default

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,14 +1,12 @@
-
-
-  
 // Template Map
 export default {
-  '/Users/zengzhi/Projects/user-list/src/pages/404.js': require('/Users/zengzhi/Projects/user-list/src/pages/404.js').default,
-'/Users/zengzhi/Projects/user-list/src/pages/about.js': require('/Users/zengzhi/Projects/user-list/src/pages/about.js').default,
-'/Users/zengzhi/Projects/user-list/src/pages/blog.js': require('/Users/zengzhi/Projects/user-list/src/pages/blog.js').default,
-'/Users/zengzhi/Projects/user-list/src/pages/index.js': require('/Users/zengzhi/Projects/user-list/src/pages/index.js').default,
-'/Users/zengzhi/Projects/user-list/src/containers/Post': require('/Users/zengzhi/Projects/user-list/src/containers/Post').default
-}
+  "/Users/zengzhi/Projects/user-list/src/pages/404.js": require("/Users/zengzhi/Projects/user-list/src/pages/404.js")
+    .default,
+  "/Users/zengzhi/Projects/user-list/src/pages/about.js": require("/Users/zengzhi/Projects/user-list/src/pages/about.js")
+    .default,
+  "/Users/zengzhi/Projects/user-list/src/pages/index.js": require("/Users/zengzhi/Projects/user-list/src/pages/index.js")
+    .default
+};
 
-export const notFoundTemplate = '/Users/zengzhi/Projects/user-list/src/pages/404.js'
-
+export const notFoundTemplate =
+  "/Users/zengzhi/Projects/user-list/src/pages/404.js";
