4d76a7980b1a3eaa6baa68544719e62431016554
zhi zeng <cursorzz@163.com>
2020-01-13 08:25:54 +0800

    artifacts will auto append post

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,12 +1,12 @@
+
+
+  
 // Template Map
 export default {
-  "/Users/zengzhi/Projects/user-list/src/pages/404.js": require("/Users/zengzhi/Projects/user-list/src/pages/404.js")
-    .default,
-  "/Users/zengzhi/Projects/user-list/src/pages/about.js": require("/Users/zengzhi/Projects/user-list/src/pages/about.js")
-    .default,
-  "/Users/zengzhi/Projects/user-list/src/pages/index.js": require("/Users/zengzhi/Projects/user-list/src/pages/index.js")
-    .default
-};
+  '/Users/zengzhi/Projects/user-list/src/pages/404.js': require('/Users/zengzhi/Projects/user-list/src/pages/404.js').default,
+'/Users/zengzhi/Projects/user-list/src/pages/about.js': require('/Users/zengzhi/Projects/user-list/src/pages/about.js').default,
+'/Users/zengzhi/Projects/user-list/src/pages/index.js': require('/Users/zengzhi/Projects/user-list/src/pages/index.js').default
+}
 
-export const notFoundTemplate =
-  "/Users/zengzhi/Projects/user-list/src/pages/404.js";
+export const notFoundTemplate = '/Users/zengzhi/Projects/user-list/src/pages/404.js'
+
--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index'
+import React from "react";
+import { Root, Routes, addPrefetchExcludes } from "react-static";
+import { Link, Router } from "components/Router";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
 
-import './app.css'
-import { Provider } from 'react-redux';
+import "./app.css";
+import { Provider } from "react-redux";
 
 const Store = createStore(rootReducer, applyMiddleware(thunk));
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
+addPrefetchExcludes(["dynamic"]);
 
 function App() {
   return (
@@ -24,13 +23,13 @@ function App() {
         <div className="content">
           <React.Suspense fallback={<em>Loading...</em>}>
             <Router>
-              <Dynamic path="dynamic" />
               <Routes path="*" />
             </Router>
           </React.Suspense>
         </div>
       </Root>
     </Provider>
-  )
+  );
 }
-export default App
+export default App;
+
